440e10a6c9dc3a88cd4ccd80890785ce5d7fedba
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-10-27 00:09:52 -0500

    Interaction with tokens obtained, rough work not , not organized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Scene from "./Scene";
+
+function find(e) {
+  console.log(e.target);
 }
 
-export default App;
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows flat linear>
+        <Scene />
+        <OrbitControls />
+      </Canvas>
+    </Suspense>
+  );
+}
---
+++ src/Game.js
@@ -0,0 +1,9 @@
+
+export default function Game(props){
+    console.log(props.state)
+
+    const scene = props.state.scene
+    // object containing information of each player
+
+
+}
