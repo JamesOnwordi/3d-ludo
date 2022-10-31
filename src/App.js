6b9ab09cbb2b0b158d30d5f82c46a249248cd40f
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-10-30 19:26:33 -0500

    almost done with the display for users

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Nav from "./Nav";
 
 import Scene from "./Scene";
-
-function find(e) {
-  console.log(e.target);
-}
 
 export default function App() {
   return (
     <Suspense fallback={null}>
+      <div className="displayBox">
+        <h1 id="display">Yellow turn, roll dice to play</h1>
+      </div>
       <Canvas shadows flat linear>
         <Scene />
         <OrbitControls />
       </Canvas>
+      <Nav />
+      <div className="displayBox">hidden</div>
     </Suspense>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+export default function Nav(){
+    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "265px";
+        document.getElementById("main").style.marginLeft = "265px";
+        console.log(document.getElementsByClassName("openbtn"))
+        document.getElementById("open").style.visibility = "hidden"
+        
+    }
+    
+    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("open").style.visibility = "visible"
+    }
+    return(
+        <div className="absolute">
+            <div id="mySidebar" class="sidebar">
+                <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
+                <a href="#">Resume</a>
+                <a href="#">Reset</a>
+                <a href="#">Instructions</a>
+                <a href="#">About</a>
+                </div>
+
+                <div id="main">
+                <button class="openbtn" id="open" onClick={openNav} onTouchMove={openNav}>&#9776;</button>
+            </div>
+        </div>
+    )
+}
